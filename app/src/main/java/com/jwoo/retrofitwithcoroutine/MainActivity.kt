b053936fdb3de008cf72c31a760b6912570da3a4
John Woo <jdwoo.88@yahoo.com>
2020-06-11 01:32:53 +0800

    - Added Logging Service - Used @Query and @Path hint

--- app/src/main/java/com/jwoo/retrofitwithcoroutine/MainActivity.kt
+++ app/src/main/java/com/jwoo/retrofitwithcoroutine/MainActivity.kt
@@ -4,33 +4,63 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retrofitService : AlbumService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        //getRequestWithQueryParameters()
+        getRequestWithParameters()
+    }
+
+    private fun getRequestWithQueryParameters(){
 
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retrofitService.getAlbums()
+            // val response = retrofitService.getSortedAlbums(1)
             emit(response)
         }
 
         responseLiveData.observe(this, Observer {
             val albumsList: MutableListIterator<AlbumsItem>? = it.body()?.listIterator()
             if (albumsList != null){
-                var counter : Int = 1
                 while(albumsList.hasNext()){
                     val albumsItem = albumsList.next();
-                    Log.d("JWOOLOG", "Counter: $counter Title: " + albumsItem.title)
-                    counter++
+                    Log.d("JWOOLOG", "Title: " + albumsItem.title)
+                    val result =    "Album Id: ${albumsItem.id}\n" +
+                                    "Album Title: ${albumsItem.title}\n" +
+                                    "Album User: ${albumsItem.userId}\n\n\n"
+
+                    text_view.append(result)
                 }
             }
+        })
+    }
+
+    private fun getRequestWithParameters(){
+        // path parameter sample
+        val responseAlbumItemLiveData: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retrofitService.getAlbum(1)
+            //val response = retrofitService.getSortedAlbums(1)
+            emit(response)
+        }
+
+        responseAlbumItemLiveData.observe(this, Observer {
+            val title: String? = it.body()?.title
+            Log.d("JWOOLOG", "Title: " + title)
+            val result =    "Album Title: $title"
+            text_view.append(result)
         })
     }
 }
--- app/src/main/java/com/jwoo/retrofitwithcoroutine/RetrofitInstance.kt
+++ app/src/main/java/com/jwoo/retrofitwithcoroutine/RetrofitInstance.kt
@@ -1,16 +1,35 @@
 package com.jwoo.retrofitwithcoroutine
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 class RetrofitInstance {
+
     companion object {
         val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
 
         fun getRetrofitInstance(): Retrofit {
+
+            val interceptor = HttpLoggingInterceptor().apply {
+                this.level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val okHttpClient = OkHttpClient.Builder().apply {
+                this.addInterceptor(interceptor)
+            }
+            //                .connectTimeout(60, TimeUnit.SECONDS)
+            //                .writeTimeout(60, TimeUnit.SECONDS)
+            //                .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
