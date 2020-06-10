fdeda76d70ac4ece74097fe0a3e356165e9447b9
John Woo <jdwoo.88@yahoo.com>
2020-06-10 19:54:46 +0800

    - First Retrofit call

--- app/src/main/java/com/jwoo/retrofitwithcoroutine/MainActivity.kt
+++ app/src/main/java/com/jwoo/retrofitwithcoroutine/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.jwoo.retrofitwithcoroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList: MutableListIterator<AlbumsItem>? = it.body()?.listIterator()
+            if (albumsList != null){
+                var counter : Int = 1
+                while(albumsList.hasNext()){
+                    val albumsItem = albumsList.next();
+                    Log.d("JWOOLOG", "Counter: $counter Title: " + albumsItem.title)
+                    counter++
+                }
+            }
+        })
     }
 }
